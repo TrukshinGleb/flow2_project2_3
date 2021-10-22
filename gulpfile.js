ff7cad246976456dd5cf77b45deeff483b2096e8
TrukshinGleb <gleb710777@gmail.com>
2021-10-23 00:31:04 +0300

    gulp trouble

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,37 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("gulp-autoprefixer");
-var csso = require("gulp-csso");
-var rename = require("gulp-rename")
+const gulp = require ('gulp');
+const browserSync = require ('browser-sync').create();
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('html', () => {
-
+gulp.task('html', function() {
     return gulp.src('source/*.html')
-      .pipe(gulp.dest('./build'));
-  
-  });  
+        .pipe(gulp.dest('./build'));
+});
 
 gulp.task('watch', () => {
     browserSync.init({
       server: {
-        baseDir: "./build"
+        baseDir: "build"
       }
     });
   
-    gulp.watch('./source/*.html', gulp.series('html'))
-    gulp.watch("./source/*.html").on('change', browserSync.reload);
+    gulp.watch('source/*.html', gulp.series('html'))
+    gulp.watch("source/*.html").on('change', browserSync.reload);
   });
 
-  gulp.task("css", function () {
-    return gulp.src("source/scss/style.scss")
-      .pipe(plumber())
-      .pipe(sourcemap.init())
-      .pipe(sass())
-      .pipe(postcss([
-        autoprefixer()
-      ]))
-      .pipe(csso())
-      .pipe(rename("style.min.css"))
-      .pipe(sourcemap.write("."))
-      .pipe(gulp.dest("build/css"));
-  });
+gulp.task("css", function (){
+  return gulp.src("source/scss/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("duild/css"))
+});  
  
 
 
